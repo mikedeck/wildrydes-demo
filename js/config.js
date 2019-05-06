b8d3bc329cdef07dd48fc3a6b07d11305f53c83c
Mike Deck <mikedeck@amazon.com>
2019-05-06 11:41:48 -0500

    updating config with User Pool settings

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_zwudbW4pm', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7bj3pim2ol8t27je8c8v1ffrr4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
